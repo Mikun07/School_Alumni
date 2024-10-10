feae226586cb26d6f2dabd85af476672c67d8ea7
Festus-Olaleye Ayomikun <76745046+Mikun07@users.noreply.github.com>
2024-10-10 14:10:25 +0200

    fix

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -60,52 +60,60 @@ const AboutPage = () => {
               />
             </div>
           </div>
+
           <div className=" lg:w-[50%] w-full flex flex-col gap-3 items-center">
             <h1 className="gradientText capitalize font-bold text-[25px] w-full leading-8 tracking-wide text-center">
               Federal Government College Odogbolu, '99
             </h1>
 
-            <p className="text-justify text-xs text-gray-600">
-              Established in [Year], Federal Government College Odogbolu has
-              been a beacon of academic excellence and personal growth. Our
-              alumni community, formed by the graduating class of 1999, proudly
-              represents the values and ideals instilled in us during our
-              formative years at the college. Our mission is to foster a strong
-              network of alumni, support our alma mater, and contribute
-              positively to society. We believe in the power of collaboration
-              and mentorship, and our association serves as a platform for
-              members to connect, share experiences, and support one another.
-              Our Vision To create a thriving alumni community that upholds the
-              legacy of Federal Government College Odogbolu, empowering future
-              generations through mentorship, collaboration, and philanthropic
-              initiatives. Our Values
-              <ul className="list-disc list-inside ">
-                <li>
-                  <b>Community:</b>
-                  We believe in the strength of our network and aim to create a
-                  supportive environment for all members.
-                </li>
-                <li>
-                  <b>Integrity:</b>
-                  Integrity: We uphold the highest ethical standards in our
-                  actions and interactions.
-                </li>
-                <li>
-                  <b>Excellence:</b> We strive for excellence in all our
-                  endeavors,
-                </li>
-              </ul>
-              reflecting the values we learned at FGC Odogbolu. Get Involved We
-              invite all alumni to join us in our mission. Whether you are
-              looking to reconnect with old friends, mentor current students, or
-              contribute to community projects, there are many ways to get
-              involved. Stay updated on our events, initiatives, and news by
-              following us on our social media platforms and subscribing to our
-              newsletter. Together, we can continue to make a positive impact in
-              our communities and beyond. Thank you for being a part of the
-              Federal Government College Odogbolu, '99 alumni family. We look
-              forward to connecting with you!
-            </p>
+            <div className="text-justify text-xs text-gray-600">
+              <p>
+                Established in [Year], Federal Government College Odogbolu has
+                been a beacon of academic excellence and personal growth. Our
+                alumni community, formed by the graduating class of 1999,
+                proudly represents the values and ideals instilled in us during
+                our formative years at the college. Our mission is to foster a
+                strong network of alumni, support our alma mater, and contribute
+                positively to society. We believe in the power of collaboration
+                and mentorship, and our association serves as a platform for
+                members to connect, share experiences, and support one another.
+                Our Vision To create a thriving alumni community that upholds
+                the legacy of Federal Government College Odogbolu, empowering
+                future generations through mentorship, collaboration, and
+                philanthropic initiatives.
+              </p>
+              <p className="px-2">
+                Our Values
+                <ul className="list-disc list-inside ">
+                  <li>
+                    <b>Community:</b>
+                    We believe in the strength of our network and aim to create
+                    a supportive environment for all members.
+                  </li>
+                  <li>
+                    <b>Integrity:</b>
+                    Integrity: We uphold the highest ethical standards in our
+                    actions and interactions.
+                  </li>
+                  <li>
+                    <b>Excellence:</b> We strive for excellence in all our
+                    endeavors,
+                  </li>
+                </ul>
+              </p>
+              <p className="">
+                reflecting the values we learned at FGC Odogbolu. Get Involved
+                We invite all alumni to join us in our mission. Whether you are
+                looking to reconnect with old friends, mentor current students,
+                or contribute to community projects, there are many ways to get
+                involved. Stay updated on our events, initiatives, and news by
+                following us on our social media platforms and subscribing to
+                our newsletter. Together, we can continue to make a positive
+                impact in our communities and beyond. Thank you for being a part
+                of the Federal Government College Odogbolu, '99 alumni family.
+                We look forward to connecting with you!
+              </p>
+            </div>
           </div>
         </div>
       </div>
